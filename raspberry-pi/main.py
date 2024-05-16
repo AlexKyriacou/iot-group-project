c2a252073f8f313987465381482c26249e104b89
Alex Kyriacou <a.kyriacou14@gmail.com>
2024-05-16 12:31:47 +1000

    revert mosquitto

--- raspberry-pi/main.py
+++ raspberry-pi/main.py
@@ -11,34 +11,51 @@ def signal_handler(signal, frame):
     Handle signals to gracefully exit the program.
     """
     print("Exiting program...")
-    mqtt_client.loop_stop()
-    mqtt_client.disconnect()
+    thingsboard_client.loop_stop()
+    thingsboard_client.disconnect()
+    mosquitto_client.loop_stop()
+    mosquitto_client.disconnect()
     exit(0)
     
 def handle_mqtt_data():
-    if mqtt_client.data:
-        if "deviceType" not in mqtt_client.data:
-            print(f"Received invalid data: {mqtt_client.data}")
-            mqtt_client.data = None
+    if thingsboard_client.data:
+        if "deviceType" not in thingsboard_client.data:
+            print(f"Received invalid data: {thingsboard_client.data}")
+            thingsboard_client.data = None
             return
         
-        device_type = mqtt_client.data["deviceType"]
+        device_type = thingsboard_client.data["deviceType"]
         if device_type == "alarmSystem":
-            print(f"Received command data: {mqtt_client.data}")
-            serial_reader.send_data(mqtt_client.data)
+            print(f"Received command data: {thingsboard_client.data}")
+            serial_reader.send_data(thingsboard_client.data)
         else:
-            print(f"Received unknown data: {mqtt_client.data}")
-        mqtt_client.data = None
+            print(f"Received unknown data: {thingsboard_client.data}")
+        thingsboard_client.data = None
+    if mosquitto_client.data:
+        if "deviceType" not in mosquitto_client.data:
+            print(f"Received invalid data: {mosquitto_client.data}")
+            mosquitto_client.data = None
+            return
+        
+        device_type = mosquitto_client.data["deviceType"]
+        if device_type == "alarmSystem":
+            print(f"Received command data: {mosquitto_client.data}")
+            serial_reader.send_data(mosquitto_client.data)
+        else:
+            print(f"Received unknown data: {mosquitto_client.data}")
+        mosquitto_client.data = None
 
 def read_and_publish_sensor_data():
     sensor_data = serial_reader.read_sensor_data()
     sensor_data = serial_reader.update_data_with_device_id_and_timestamp(sensor_data)
     serialized_data = json.dumps(sensor_data)
     print(f"Publishing sensor data: {sensor_data}")
-    mqtt_client.publish(f"v1/devices/me/telemetry", serialized_data)
+    thingsboard_client.publish(f"v1/devices/me/telemetry", serialized_data)
+    mosquitto_client.publish(f"v1/devices/me/telemetry", serialized_data)
 
 def main():
-    mqtt_client.connect()
+    thingsboard_client.connect()
+    mosquitto_client.connect()
 
     while True:
         read_and_publish_sensor_data()
@@ -52,13 +69,19 @@ signal.signal(signal.SIGTERM, signal_handler)
 
 if __name__ == "__main__":
     config = Config()
-    mqtt_client = MQTTClient(
+    thingsboard_client = MQTTClient(
         broker=config.mqtt_broker,
         port=config.mqtt_port,
         topics=["alarmSystem/command"],
         client_id=config.mqtt_client_id,
         username=config.mqtt_username,
         password=config.mqtt_password,
+    )
+    mosquitto_client = MQTTClient(
+        broker="localhost",
+        port=config.mqtt_port,
+        topics=["alarmSystem/command"],
+        client_id=config.mqtt_client_id,
     )
     serial_reader = SerialReader(config)
     main()
